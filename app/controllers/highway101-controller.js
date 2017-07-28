d493a3fd9e80177ee510cd5c2717a34424b91995
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-07-28 14:37:45 -0500

    added exercise

---
+++ app/controllers/highway101-controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+roadApp.controller('101Controller', function($scope){
+  
+  $scope.highwayName = "US 101"
+  $scope.highwayDescription = "US 101 has become internationally renowned over the years in film, popular culture, and song. It is often quoted alongside other legendary roadtrip routes, such as US 66, as a recognized symbol of American culture and lifestyle."
+
+});
